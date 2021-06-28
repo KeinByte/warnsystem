3a3540dfb41f1ec028966e0247b0049a9420bfe1
KeinByte <info@damon-guenther.de>
2021-06-28 21:36:39 +0200

    Initial commit

---
+++ src/main/java/net/minesucht/warn/WarnSystem.java
@@ -0,0 +1,18 @@
+package net.minesucht.warn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author KeinByte
+ * @since 28.06.2021
+ */
+
+public class WarnSystem extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+
+    }
+}
